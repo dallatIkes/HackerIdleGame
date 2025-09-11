9629df40842153f5b646eb50cfd84e7efe09e21b
dallatIkes <samy.chaabi0@gmail.com>
2025-09-11 11:15:33 -0400

    [feat] - music management

--- game/scripts/Settings.cs
+++ game/scripts/Settings.cs
@@ -3,10 +3,20 @@ using System;
 
 public partial class Settings : Control
 {
+
+	private Label soundVolumeLabel;
+	private HSlider volumeSlider;
+	private Music audioManager;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("=== C# Settings Screen init ===");
+		audioManager = GetNode<Music>("/root/Music");
+		volumeSlider = GetNode<HSlider>("Panel/ColorRect/VBoxContainer/Sound-settings/Sound");
+		soundVolumeLabel = GetNode<Label>("Panel/ColorRect/VBoxContainer/Sound-settings/Sound-volume-label");
+
+		volumeSlider.Value = audioManager.getCurrentVolume();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,5 +28,11 @@ public partial class Settings : Control
 	{
 		GD.Print("=== Return button pressed ===");
 		GetTree().ChangeSceneToFile("res://game/scenes/MainMenu.tscn");
+	}
+
+	private void _on_sound_value_changed(float value)
+	{
+		audioManager.SetMusicVolume((float)value);
+		soundVolumeLabel.Text = "Volume : " + ((int)(value * 100)).ToString();
 	}
 }
