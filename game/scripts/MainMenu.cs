e6c775ee72485a8518212c5aff525b1ceccaca49
dallatIkes <samy.chaabi0@gmail.com>
2025-09-10 23:02:34 -0400

    [test] - testing basic cs script

---
+++ game/scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	public override void _Ready()
+	{
+		GD.Print("=== Project Start ===");
+		var startButton = GetNode<Button>("StartButton");
+		startButton.Pressed += OnStartButtonPressed;
+	}
+	
+	private void OnStartButtonPressed()
+	{
+		GD.Print("Start Button pressed!");
+	}
+}
