a25a1bab550a478b4185fff9564f57811a61e467
dallatIkes <samy.chaabi0@gmail.com>
2025-09-10 23:25:42 -0400

    [fix] - cs works!

--- game/scripts/MainMenu.cs
+++ game/scripts/MainMenu.cs
@@ -3,16 +3,29 @@ using System;
 
 public partial class MainMenu : Control
 {
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		/* DEBUG */
-		GD.Print("=== Project Start ===");
-		var startButton = GetNode<Button>("StartButton");
-		startButton.Pressed += OnStartButtonPressed;
+		GD.Print("=== C# project init ===");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
 	}
 	
-	private void OnStartButtonPressed()
+	private void _on_start_button_pressed()
 	{
-		GD.Print("Start Button pressed!");
+		GD.Print("=== Start button pressed ===");
+	}
+	
+	private void _on_settings_button_pressed()
+	{
+		GD.Print("=== Settings button pressed ===");
+	}
+	
+	private void _on_quit_button_pressed()
+	{
+		GD.Print("=== Quit button pressed ===");
 	}
 }
